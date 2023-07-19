bb3e5084cf0cd0dd8e8f02ddf53de5ecc7b4b72d
Saman-7 <saman7dev@gmail.com>
2023-07-19 08:37:30 +0330

    Create & add layout component to all pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,25 +1,28 @@
 import React from "react";
+import Layout from "../components/Layout";
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Gatsby is a React-based open-source framework for creating websites and
-        apps. It's great whether you're building a portfolio site or blog, or a
-        high-traffic e-commerce store or company homepage.
-      </p>
-      <p>
-        Gatsby is a React-based open-source framework for creating websites and
-        apps. It's great whether you're building a portfolio site or blog, or a
-        high-traffic e-commerce store or company homepage.
-      </p>
-      <p>
-        Gatsby is a React-based open-source framework for creating websites and
-        apps. It's great whether you're building a portfolio site or blog, or a
-        high-traffic e-commerce store or company homepage.
-      </p>
-    </div>
+    <Layout>
+      <div>
+        <h1>About</h1>
+        <p>
+          Gatsby is a React-based open-source framework for creating websites
+          and apps. It's great whether you're building a portfolio site or blog,
+          or a high-traffic e-commerce store or company homepage.
+        </p>
+        <p>
+          Gatsby is a React-based open-source framework for creating websites
+          and apps. It's great whether you're building a portfolio site or blog,
+          or a high-traffic e-commerce store or company homepage.
+        </p>
+        <p>
+          Gatsby is a React-based open-source framework for creating websites
+          and apps. It's great whether you're building a portfolio site or blog,
+          or a high-traffic e-commerce store or company homepage.
+        </p>
+      </div>
+    </Layout>
   );
 };
 
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Layout from "../../components/Layout";
 
 const Projects = () => {
   return (
-    <div>
-      <h2>Portfolio</h2>
-      <h3>Projects & Website l've Created</h3>
-    </div>
+    <Layout>
+      <div>
+        <h2>Portfolio</h2>
+        <h3>Projects & Website l've Created</h3>
+      </div>
+    </Layout>
   );
 };
 
