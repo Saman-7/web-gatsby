465f01a18def2d14b28def4c3800dd4fd8461297
Saman-7 <saman7dev@gmail.com>
2023-07-19 19:37:05 +0330

    Modify footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer>Copyright 2023 Web Warrior</footer>;
+  return <footer>Copyright 2023 Web Gatsby</footer>;
 };
 
 export default Footer;
