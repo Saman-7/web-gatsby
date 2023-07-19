2bb580f172982b52a3b467b64184d73d2f7a97db
Saman-7 <saman7dev@gmail.com>
2023-07-19 14:21:58 +0330

    Add style to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,20 @@
 import * as React from "react";
 import Layout from "../components/Layout";
 import "../styles/global.css";
+import { header, btn } from "../styles/home.module.css";
+import { Link } from "@reach/router";
 
 const Home = () => {
   return (
     <Layout>
-      <section>
+      <section className={header}>
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
           <p>UX designer & web developer based in Manchester.</p>
+          <Link className={btn} to="/projects">
+            My Portfolio Projects
+          </Link>
         </div>
       </section>
     </Layout>
