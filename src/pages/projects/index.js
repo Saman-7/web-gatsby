bff19ef98630424e684a6188b652d97a6f98cd9e
Saman-7 <saman7dev@gmail.com>
2023-07-19 14:36:47 +0330

    Add style to projects page

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import { portfolio } from "../../styles/projects.module.css";
 
 const Projects = () => {
   return (
     <Layout>
-      <div>
+      <div className={portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects & Website l've Created</h3>
       </div>
