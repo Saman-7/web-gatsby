236911799b2c1f06ebaf84444e2f763061b625c1
Saman-7 <saman7dev@gmail.com>
2023-07-19 14:59:11 +0330

    Add image to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,7 @@ const Home = () => {
             My Portfolio Projects
           </Link>
         </div>
+        <img src="/banner.png" alt="site banner" style={{ maxWidth: "100%" }} />
       </section>
     </Layout>
   );
