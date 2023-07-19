7cd156269203e51f3bd41ff67182d923c16a6383
Saman-7 <saman7dev@gmail.com>
2023-07-19 07:40:02 +0330

    Add home & about page

---
+++ src/pages/about.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Gatsby is a React-based open-source framework for creating websites and
+        apps. It's great whether you're building a portfolio site or blog, or a
+        high-traffic e-commerce store or company homepage.
+      </p>
+      <p>
+        Gatsby is a React-based open-source framework for creating websites and
+        apps. It's great whether you're building a portfolio site or blog, or a
+        high-traffic e-commerce store or company homepage.
+      </p>
+      <p>
+        Gatsby is a React-based open-source framework for creating websites and
+        apps. It's great whether you're building a portfolio site or blog, or a
+        high-traffic e-commerce store or company homepage.
+      </p>
+    </div>
+  );
+};
+
+export default About;
