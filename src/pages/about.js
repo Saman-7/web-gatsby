a409e8f3a7da65f4c993726b46c7e20ca6b0211e
Saman-7 <saman7dev@gmail.com>
2023-07-22 22:44:58 +0330

    Add styles to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,25 +1,34 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { about } from "../styles/about.module.css";
 
 const About = () => {
   return (
     <Layout>
-      <div>
-        <h1>About</h1>
+      <div className={about}>
+        <h1>The fastest frontend</h1>
+        <h1>for the modern web</h1>
         <p>
-          Gatsby is a React-based open-source framework for creating websites
-          and apps. It's great whether you're building a portfolio site or blog,
-          or a high-traffic e-commerce store or company homepage.
+          With Gatsby 3.0 for React, create blazing fast web experiences that
+          integrate your favorite services and content. Build e-commerce sites,
+          landing pages, blogs and more in minutes.
         </p>
+        <h1>Develop, Build, Preview, Deploy.</h1>
+        <h1>In the time it takes to create a mock up.</h1>
         <p>
-          Gatsby is a React-based open-source framework for creating websites
-          and apps. It's great whether you're building a portfolio site or blog,
-          or a high-traffic e-commerce store or company homepage.
+          Go from idea to production in literal minutes with a 10x developer
+          experience. Gatsby is built on popular, proven tech like React and
+          GraphQL, has loads of tutorials and starter templates, and over 2500
+          plugins that can help integrate all of your favorite web services.
         </p>
+        <h1>Gatsby is unbelievably fast & smooth.</h1>
+        <h1>Everything just feels...there.</h1>
         <p>
-          Gatsby is a React-based open-source framework for creating websites
-          and apps. It's great whether you're building a portfolio site or blog,
-          or a high-traffic e-commerce store or company homepage.
+          Gatsby loads only critical 1 parts of the page, exactly when the
+          visitor needs it, and not a second sooner. Once loaded, Gatsby
+          prefetches resources 2 for other pages so that clicking on the site
+          feels buttery smooth and responsive. Everything is statically
+          generated at runtime - so your site loads as fast as possible.
         </p>
       </div>
     </Layout>
